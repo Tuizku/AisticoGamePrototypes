904e25ecb32297ab6ebb22f45df9aae2fed995aa
Tuizku <tuisku.kahra@gmail.com>
2023-08-25 14:07:30 +0300

    bugi korjausta yms yms

--- Sanapeli v.0.3/srv/funcs.gd
+++ Sanapeli v.0.3/srv/funcs.gd
@@ -8,13 +8,13 @@ static func chr_amount_in(chr, word):
 	return result
 
 static func single_chrs_in(word):
-	var result : Array
+	var result : Array = []
 	for i in len(word):
 		if not word[i] in result and chr_amount_in(word[i], word) == 1: result.append(word[i])
 	return result
 
 static func chrs_in(word):
-	var chrs : Array
+	var chrs : Array = []
 	for i in len(word):
 		if word[i] != "" and not word[i] in chrs: chrs.append(word[i])
 	return len(chrs)
@@ -25,10 +25,14 @@ static func index_of(chr, word):
 		if word[i] == chr: result = i
 	return result
 
-#static func random_new_chr_from_str(shown_chrs, word):
-	#var rng = RandomNumberGenerator.new()
-	#rng.randomize()
-	#var loop = true
-	#while loop:
-		#var index = rng.randi_range(0, len(word) - 1)
-		#if 
+const correct_sentences = ["Hyvin löydetty!", "Hyvää työtä!", "Oikein!", "Mahtavaa!", "Hienoa!", "Mahtavaa päättelyä!"]
+static func random_correct_sentence():
+	var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+	rng.randomize()
+	return correct_sentences[rng.randi_range(0, len(correct_sentences) - 1)]
+
+const wrong_sentences = ["Väärin!", "Ensi kerralla parempi onni!", "Ei osunut tällä kertaa...", "Ei aivan..."]
+static func random_wrong_sentence():
+	var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+	rng.randomize()
+	return wrong_sentences[rng.randi_range(0, len(wrong_sentences) - 1)]
--- Sanapeli v.0.3/srv/game_controller.gd
+++ Sanapeli v.0.3/srv/game_controller.gd
@@ -1,16 +1,18 @@
 extends Control
 
 # Exports
-export var GameTime : float = 30
+export var GameTime : float = 29
 
 # Data and Nodes
 var wordsData
+var usedWords : Array = []
 var letterRarity : Array = ["aitneslo", "kuämvr", "jhypdögbfcwåqxz"]
 var middleText : RichTextLabel
+var rng : RandomNumberGenerator
 
 # Whole Game Variables
 var difficulty : float = 0.5 # between 0 and 1 (1 is hardest)
-var wordsPlayed : int = 0
+var playingWordNum : int = 0
 var points : Array
 
 # One Game Variables
@@ -25,8 +27,8 @@ var boxSelectedTime : float = 0
 var timeLeft : float = 0
 
 # Signals
-signal word_created(_hint)
-signal chrs_created(_chrs)
+signal word_created(_hint) # Creates the char boxes and adds the characters
+signal chrs_created(_chrs) # Only adds the characters to boxes
 signal char_box_selected(_box_index)
 signal editing_char_selected(_index)
 signal char_chosen(_char, _index)
@@ -52,86 +54,134 @@ func new_word():
 	emit_signal("word_created", word_start_chrs)
 	emit_signal("chrs_created", chrs)
 
-	# Setup word
+	# Adds the start characters and empty spaces to "word"
 	word = ""
 	for i in len(word_start_chrs): 
 		if word_start_chrs[i] != "": word += word_start_chrs[i]
 		else: word += " "
 	check_word() # Finds the editingCharIndex
 	
-	# Hint
-	if wordsPlayed == 0:
-		middleText.bbcode_text = "[center]" + str(selectedWord["definition"])
-	wordsPlayed += 1
+	playingWordNum += 1
 func select_word():
-	# Word
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	var index = rng.randi_range(0, len(wordsData) - 1)
+	# Selects a random word from data
+	var index
+	while true:
+		index = rng.randi_range(0, len(wordsData) - 1)
+		if not index in usedWords: break
 	selectedWord = wordsData[index]
+	usedWords.append(index)
 func select_chrs():
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	
 	# Clear old chr data
 	word_start_chrs.clear()
 	chrs.clear()
 	
-	# Select the minimum amount of word start characters
+	
+	# Select The Minimum Amount Of Word Start Characters
 	# (because if word has more than 4 different chrs, it can't be typed)
+	
 	for i in len(selectedWord["word"]): word_start_chrs.append("")
-	
 	var word_chrs_amount = funcs.chrs_in(selectedWord["word"])
-	if word_chrs_amount > 4: # If there are more than 4 chars in word, show chars in word until 4 are left
-		var single_chrs : Array = funcs.single_chrs_in(selectedWord["word"])
-		for i in word_chrs_amount - 4:
-			var index = rng.randi_range(0, len(single_chrs) - 1)
-			var chrPos = funcs.index_of(single_chrs[index], selectedWord["word"])
-			word_start_chrs[chrPos] = single_chrs[index]
-			single_chrs.pop_at(index)
+	var single_chrs : Array
+	if word_chrs_amount > 4: single_chrs = funcs.single_chrs_in(selectedWord["word"])
 	
-	# Add random word start characters
+	# If there are more than 4 different chars in word, show chars in word until 4 are left 
+	for i in word_chrs_amount - 4:
+		var index = rng.randi_range(0, len(single_chrs) - 1)
+		var chrPos = funcs.index_of(single_chrs[index], selectedWord["word"])
+		word_start_chrs[chrPos] = single_chrs[index]
+		single_chrs.pop_at(index)
+	
+	
+	
+	# Add Random Word Start Characters By Using Difficulty
+	
 	var add_chars_amount = int((len(selectedWord["word"]) - 1) * (1 - difficulty))
-	#print(add_chars_amount)
 	add_chars_amount -= funcs.chrs_in(word_start_chrs)
-	#print(add_chars_amount)
-	#print(word_start_chrs)
 	for i in add_chars_amount:
-		var loop = true
-		while loop:
+		while true: # loops until a character has been added to a random and empty space
 			var index = rng.randi_range(0, len(selectedWord["word"]) - 1)
 			var chr = selectedWord["word"][index]
 			if word_start_chrs[index] == "": 
 				word_start_chrs[index] = chr
-				loop = false
+				break
 	
-	# Select the usable characters
+	
+	
+	# Select The Usable Characters
+	
+	# Add characters that you need when filling the word
 	for i in len(selectedWord["word"]):
 		if not selectedWord["word"][i] in chrs and word_start_chrs[i] == "":
 			chrs.append(selectedWord["word"][i])
+	
+	# Adds random characters, if there aren't enough
 	for i in 4 - len(chrs):
-		var randomChr = char(rng.randi_range(ord("a"), ord("z")))
-		chrs.append(randomChr)
+		while true: # loops until a new different char has been added to chars
+			var randomChr = char(rng.randi_range(ord("a"), ord("z")))
+			if not randomChr in chrs:
+				chrs.append(randomChr)
+				break
 	chrs.shuffle()
 func check_word():
 	# Checks the word, if it still has spaces, it selects a new editingCharIndex
 	# Else it will check if the word is included in words list
+	
+	# Tries to find an empty space in word and set editingCharIndex there
 	for i in word.length():
 		if word[i] == " ":
 			editingCharIndex = i
 			emit_signal("editing_char_selected", editingCharIndex)
 			return
-	# if word is filled, code gets here
+	
+	# If the word is filled, code gets here
 	editingCharIndex = -1
 	emit_signal("editing_char_selected", editingCharIndex)
+	cutscene()
+func cutscene():
+	# START OF CUTSCENE
+	
 	add_points()
 	change_difficulty()
-	cutscene()
+	editingWord = false
+	var time = max(0, timeLeft - 3)
 	
-	#if word == selectedWord["word"]:
-		#show_text("Oikein!", Color.green)
-	#else: 
-		#show_text("Väärin!", Color.lightcoral)
+	# Show the correct word and if you were correct
+	emit_signal("hide_boxes", "sel")
+	emit_signal("word_created", selectedWord["word"])
+	if points[len(points) - 1] != 0:
+		middleText.modulate = Color.green
+		middleText.bbcode_text = "[center]" + funcs.random_correct_sentence()
+	else:
+		middleText.modulate = Color.lightcoral
+		middleText.bbcode_text = "[center]" + funcs.random_wrong_sentence()
+	yield(get_tree().create_timer(3), "timeout")
+	
+	
+	
+	# MIDDLE OF CUTSCENE (new word and possibly an early hint)
+	var parent : BoxContainer = middleText.get_parent()
+	middleText.modulate = Color.white
+	
+	# if early hint cutscene would take more than 1 seconds, display it
+	if time >= 1:
+		new_word()
+		parent.alignment = BoxContainer.ALIGN_BEGIN
+		middleText.bbcode_text = "[center]" + selectedWord["definition"]
+		emit_signal("hide_boxes", "word")
+		yield(get_tree().create_timer(time), "timeout")
+		
+	# there wasn't time for early hint, so just show the next word
+	else:
+		yield(get_tree().create_timer(time), "timeout")
+		new_word()
+		middleText.bbcode_text = "[center]" + selectedWord["definition"]
+	
+	
+	
+	# END OF CUTSCENE
+	parent.alignment = BoxContainer.ALIGN_CENTER
+	editingWord = true
+	emit_signal("show_boxes", "all")
 func add_points():
 	points.append(0)
 	if word == selectedWord["word"]:
@@ -140,7 +190,8 @@ func add_points():
 			if word_start_chrs[i] == "":
 				if word[i] in letterRarity[0]: points[pointsIndex] += 5
 				elif word[i] in letterRarity[1]: points[pointsIndex] += 10
-				elif word[i] in letterRarity[2]: points[pointsIndex] += 25
+				elif word[i] in letterRarity[2]: points[pointsIndex] += 15
+		points[pointsIndex] += int(timeLeft / 2)
 func change_difficulty():
 	if word == selectedWord["word"]:
 		difficulty += 0.003 * timeLeft * len(word)
@@ -151,21 +202,32 @@ func change_difficulty():
 
 
 func _ready():
-	middleText = get_node("Middle Text Container").get_node("Middle Text") as RichTextLabel
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
 	load_words()
 	new_word()
 	timeLeft = GameTime
+	
+	middleText = get_node("Middle Text Container").get_node("Middle Text") as RichTextLabel
+	middleText.bbcode_text = "[center]" + str(selectedWord["definition"])
 
 func _physics_process(delta: float) -> void:
-	pc_inputs(delta)
+	# Run inputs if editing a word, else reset box selections to avoid bugs in next word
+	if editingWord: pc_inputs(delta)
+	elif handPos != -1:
+		boxSelectedTime = 0
+		handPos = -1
+		emit_signal("char_box_selected", int(handPos))
 	
 	timeLeft -= delta
 	emit_signal("change_time", 1 - timeLeft / GameTime)
 	
+	# Start a new word by calling cutscene
 	if (timeLeft <= 0):
 		if editingWord == true:
-			add_points()
-			change_difficulty()
+			editingCharIndex = -1
+			emit_signal("editing_char_selected", editingCharIndex)
 			cutscene()
 		timeLeft = GameTime
 
@@ -191,36 +253,6 @@ func pc_inputs(delta_param : float):
 		handPos = -1
 		emit_signal("char_box_selected", int(handPos))
 
-func show_text(text, color):
-	middleText.modulate = color
-	middleText.bbcode_text = "[center]" + text
-	yield(get_tree().create_timer(5.0), "timeout")
-	middleText.bbcode_text = ""
-	middleText.modulate = Color.white
 
-func cutscene():
-	var time = max(0, timeLeft - 3)
-	editingWord = false
-	emit_signal("hide_boxes", "sel")
-	var correctWord : Array
-	#for i in len(selectedWord["word"]): correctWord.append(selectedWord["word"][i])
-	emit_signal("word_created", selectedWord["word"])
-	if points[len(points) - 1] != 0:
-		middleText.modulate = Color.green
-		middleText.bbcode_text = "[center]Hyvin löydetty!"
-	else:
-		middleText.modulate = Color.lightcoral
-		middleText.bbcode_text = "[center]Väärin!"
-	yield(get_tree().create_timer(3), "timeout")
-	
-	new_word()
-	var parent : BoxContainer = middleText.get_parent()
-	if time >= 1: parent.alignment = BoxContainer.ALIGN_BEGIN
-	middleText.modulate = Color.white
-	middleText.bbcode_text = "[center]" + selectedWord["definition"]
-	emit_signal("hide_boxes", "word")
-	yield(get_tree().create_timer(time), "timeout")
-	parent.alignment = BoxContainer.ALIGN_CENTER
-	
-	editingWord = true
-	emit_signal("show_boxes", "all")
+
+
